57d1642d262ea7a6b976e90e6b9bb05e22d785b9
CraigsterJ <craigj@codemk.com>
2018-06-14 10:26:17 -0700

    Setup new roles models after adding to database

---
+++ ConnectWeb/Models/DataModels/Role.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConnectWeb.Models
+{
+    public partial class Role
+    {
+        public int Id { get; set; }
+        public Guid RoleId { get; set; }
+        public int ApplicationId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool Deleted { get; set; }
+    }
+}
---
+++ ConnectWeb/Models/MetaDataModels/RoleMetaData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Mvc;
+namespace ConnectWeb.Models
+{
+    
+    public partial class RoleMetaDataType
+    {
+        [Required(ErrorMessage = "Role name is required.")]
+        [StringLength(100, ErrorMessage = "Name can not exceed 100 characters.")]
+        public string Name { get; set; }
+
+        [StringLength(250, ErrorMessage = "Description can not exceed 250 characters.")]
+        public string Description { get; set; }
+    }
+}
